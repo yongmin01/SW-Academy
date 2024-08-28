2dff22aef50293a6a8f04ddea33fd396cb3e44a7
yongmin01 <heart77123@naver.com>
2024-08-28 09:46:55 +0900

    feat: 라우팅 설정

--- connecting/src/App.js
+++ connecting/src/App.js
@@ -1,9 +1,19 @@
-import GlobalStyle from "./styles/GlobalStyle";
-import Main from "./pages/Main";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import JoinNetworkingPage from "./pages/JoinNetworking/JoinNetworkingPage";
+import EnterInviteCodePage from "./pages/EnterInviteCode/EnterInviteCodePage";
+import EnterMyInfoPage from "./pages/EnterMyInfo/EnterMyInfoPage";
+
 function App() {
   return (
     <div className="App">
-      <Main />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/joinNetworking" element={<JoinNetworkingPage />} />
+          <Route path="/enterInviteCode" element={<EnterInviteCodePage />} />
+          <Route path="/enterMyInfo" element={<EnterMyInfoPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
